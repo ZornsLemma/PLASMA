0b838cc7fa23dc8306afc617fedb9e1ecfe94796
Steven Flintham <sgf@lemma.co.uk>
2017-01-05 21:57:36 +0000

    Tweak optimiser

--- src/optimise.py
+++ src/optimise.py
@@ -3,6 +3,12 @@ import sys
 
 
 
+# Taking precedent from SUB, which subtracts next from top from top, if we have
+# CW 2:CW 3:SUB (== CW 1), the first child of the SUB node is 3 and the second
+# is 2, not the other way around.
+
+
+
 class Node:
     def __init__(self, instruction):
         assert type(instruction) == list
@@ -14,9 +20,17 @@ class Node:
         for child in self.children:
             child.dump(level + 1)
 
+    # TODO: I think I will need a concept of "unknown value" - this will allow
+    # the emulated stack to be initialised on function entry (with the number
+    # of function parameters) or after a CALL (with a single value). (CALL,
+    # incidentally, will have to force a fresh start - we have no way to know
+    # how many arguments CALL consumes. So we would be able to optimise the
+    # instructions between CALLs, but not the CALLs themselves.)
+
     def optimise(self):
         for child in self.children:
             child.optimise()
+
         # This will optimise CB to itself, but that's harmless, and it's useful
         # to do this processing for CW, as it may allow us to switch to CB if
         # the operand is byte-sized.
@@ -29,21 +43,45 @@ class Node:
                 self.instruction = ['CW', str(value)]
             self.children = []
 
+        # If only one child is constant, we try to arrange for that to be the
+        # first child. This is helpful for other optimisations, and (I
+        # think) minimises stack use, since the constant only occupies a stack
+        # slot once the calculation of the non-constant (which may use many
+        # stack locations before collapsing down to one) is complete. If both
+        # are constant and one constant is 2, we switch that to the first
+        # child to open up possibilities for using IDXW.
+        assert len(self.children) <= 2
+        if self.is_commutative() and len(self.children) == 2:
+            if self.children[1].is_constant():
+                if (not self.children[0].is_constant() or
+                    self.children[1].evaluate() == 2):
+                        self.children[0], self.children[1] = self.children[1], self.children[0]
+                
+
     def is_constant(self):
-        assert type(self.instruction) == list
         info = opcodes[self.instruction[0]]
         return info.has_key('constant') and info['constant']
 
+    def is_commutative(self):
+        # TODO: This should probably return False *if* any of the children
+        # (direct or indirect) are the proposed 'unknown value' nodes
+        info = opcodes[self.instruction[0]]
+        return info.has_key('commutative') and info['commutative']
+
     def can_evaluate(self):
         return (all(child.is_constant() for child in self.children) and
-                self.instruction[0] in ('ADD', 'CW', 'CB'))
+                self.instruction[0] in ('CW', 'CB', 'ADD', 'SUB', 'MUL'))
 
     def evaluate(self):
         assert self.can_evaluate()
         if self.instruction[0] in ('CW', 'CB'):
             value = int(self.instruction[1])
         elif self.instruction[0] == 'ADD':
-            value = sum(child.evaluate() for child in self.children)
+            value = self.children[0].evaluate() + self.children[1].evaluate()
+        elif self.instruction[0] == 'SUB':
+            value = self.children[0].evaluate() - self.children[1].evaluate()
+        elif self.instruction[0] == 'MUL':
+            value = self.children[0].evaluate() * self.children[1].evaluate()
         else:
             assert False
         value = value & 0xffff
@@ -66,6 +104,7 @@ def tree(instructions):
         node = Node(s)
         if len(stack) < opcode_info['consume']:
             die('Stack underflow')
+        arguments = []
         for i in range(opcode_info['consume']):
             node.children.append(stack.pop())
         assert opcode_info['produce'] <= 1
@@ -79,12 +118,15 @@ def tree(instructions):
 opcodes = {
     'CB':  { 'consume' : 0, 'produce' : 1, 'constant' : True },
     'CW':  { 'consume' : 0, 'produce' : 1, 'constant' : True },
-    'ADD': { 'consume' : 2, 'produce' : 1 }
+    'ADD': { 'consume' : 2, 'produce' : 1, 'commutative' : True },
+    'SUB': { 'consume' : 2, 'produce' : 1},
+    'MUL': { 'consume' : 2, 'produce' : 1, 'commutative' : True },
+    'LW':  { 'consume' : 1, 'produce' : 1}
 }
 
 
 
-instructions = ['CW 5', 'CW 4', 'ADD', 'CW 10', 'ADD']
+instructions = ['CW 1000', 'LW', 'CB 2', 'CW 3', 'CW 5', 'SUB', 'CW 10', 'ADD', 'CW 1001', 'LW', 'ADD', 'MUL', 'ADD']
 node = tree(instructions)
 print('Before:\n')
 node.dump()
