41e2377bc5a8fe528ffd73ce31f267ebc22d98ce
Steven Flintham <sgf@lemma.co.uk>
2017-07-23 15:52:17 +0100

    Improvements to pc.py - snapshot

--- src/pc.py
+++ src/pc.py
@@ -14,10 +14,12 @@
 # order).
 
 import argparse
+import atexit
 import os
 import re
 import subprocess
 import sys
+import tempfile
 
 def die(s):
     sys.stderr.write(s + '\n')
@@ -33,7 +35,7 @@ parser.add_argument('inputs', metavar='FILE', nargs='+', help='a PLASMA source f
 args = parser.parse_args()
 
 init_list = []
-assembler_input = []
+plasm_output = {}
 
 for infile in args.inputs:
     infile_name, infile_extension = os.path.splitext(infile)
@@ -44,29 +46,26 @@ for infile in args.inputs:
         # through...
         plasm = subprocess.Popen(['./plasm', '-A'], stdin=open(infile, 'r'), stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
         # TODO: We could strip the leading JMP _INIT off the plasm output - it's redundant
-        with open(infile_name + '.sa', 'w') as outfile:
-            prefix = '_' + os.path.basename(infile_name).upper() + '_'
-            for line in plasm.stdout:
-                if line[0:5] == '_INIT':
-                    # TODO: This is a bit hacky
-                    next_line = plasm.stdout.next()
-                    if 'JSR' in next_line and 'INTERP' in next_line:
-                        line = '_INIT' + prefix
-                        init_list.append(line)
-                        outfile.write(line + '\n')
-                        line = next_line
-                    else:
-                        line = None
+        o = plasm_output[infile_name + '.sa'] = []
+        prefix = '_' + os.path.basename(infile_name).upper() + '_'
+        for line in plasm.stdout:
+            if line[0:5] == '_INIT':
+                # TODO: This is a bit hacky
+                next_line = plasm.stdout.next()
+                if 'JSR' in next_line and 'INTERP' in next_line:
+                    line = '_INIT' + prefix
+                    init_list.append(line)
+                    o.append(line + '\n')
+                    line = next_line
                 else:
-                    line = re.sub(r'\b_([ABCDFPX])', prefix + r'\1', line)
-                if line is not None:
-                    outfile.write(line)
-        infile_extension = '.sa'
-
-    if infile_extension == '.sa':
-        # TODO: We need to populate init_list - we get away with it if we just did the previous step
-        assembler_input.append(infile_name + infile_extension)
+                    line = None
+            else:
+                line = re.sub(r'\b_([ABCDFPX])', prefix + r'\1', line)
+            if line is not None:
+                o.append(line)
     else:
+        # TODO: We need to allow user-written assembler source to be specified
+        # on the command line.
         die('Unknown file extension: ' + infile)
 
 if not init_list:
@@ -88,29 +87,49 @@ if not init_list:
 # comments delimiting the various files which went into it. Let's not forget
 # that performance isn't really an issue - the extra time and disc space
 # required for the concatenated file creation is negligible.
-output_asm = 'foo' # SFTODO: need to decide what path/name to use for this
-with open(output_asm, 'w') as outfile:
-    cat(outfile, 'vmsrc/plvmbb-pre.s')
 
-    # We need to take the contents of 32cmd.sa but strip off the ZERO:RET at the end of its
-    # _INIT, so we can fall through into calling other initialisation code. TODO: A bit hacky
-    # the way we recognise this...
-    with open('vmsrc/32cmd.sa', 'r') as infile:
-        for line in infile:
-            distinctive = ': done\n'
-            if line[-len(distinctive):] == distinctive:
-                discard = infile.next()
-                discard = infile.next()
-            outfile.write(line)
+# TODO: If a -S option is given, we should use a non-temp file for the output
+# and return without assembling it.
+# TODO: Rename outfile?
+outfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+atexit.register(lambda f: os.remove(f), outfile.name)
 
-    # TODO: We don't want this, I think - 32cmd.sa has already started interpreting and we're appending to the end of its bytecode init  outfile.write('\tJSR\tINTERP\n')
-    for init in init_list:
-        outfile.write('\t!BYTE\t$54\t\t\t; CALL ' + init + '\n')
-        outfile.write('\t!WORD\t' + init + '\n')
-    # TODO: What can/should we do (perhaps nothing) to "cope" if the final init returns? (It shouldn't)
+cat(outfile, 'vmsrc/plvmbb-pre.s')
 
-    for asm in assembler_input:
-        cat(outfile, asm)
+# We need to take the contents of 32cmd.sa but strip off the ZERO:RET at the end of its
+# _INIT, so we can fall through into calling other initialisation code. TODO: A bit hacky
+# the way we recognise this...
+with open('vmsrc/32cmd.sa', 'r') as infile:
+    for line in infile:
+        distinctive = ': done\n'
+        if line[-len(distinctive):] == distinctive:
+            discard = infile.next()
+            discard = infile.next()
+        outfile.write(line)
 
-    cat(outfile, 'vmsrc/plvmbb-post.s')
+for init in init_list:
+    outfile.write('\t!BYTE\t$54\t\t\t; CALL ' + init + '\n')
+    outfile.write('\t!WORD\t' + init + '\n')
+# TODO: What can/should we do (perhaps nothing) to "cope" if the final init returns? (It shouldn't)
 
+for infile in args.inputs:
+    infile_name, infile_extension = os.path.splitext(infile)
+    infile_extension = infile_extension.lower()
+    if infile_extension == '.pla':
+        outfile.writelines(plasm_output[infile_name + '.sa'])
+    else:
+        # TODO: We need to allow user-written assembler source to be specified
+        # on the command line.
+        die('Unknown file extension: ' + infile)
+
+cat(outfile, 'vmsrc/plvmbb-post.s')
+
+outfile.close()
+
+# TODO: We should support generating relocatable standalone output by invoking
+# ACME twice and appending relocations
+
+# TODO: We need to allow our caller to specify options to pass through to ACME
+# TODO: --report needs to be optional
+acme_result = subprocess.call(['acme', '--setpc', '$2000', '-DSTART=$2000', '--report', 'hanois.lst', '-o', 'hanois', outfile.name])
+sys.exit(acme_result)
