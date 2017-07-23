fd637d88bd1421df8f7dfc952601f9472040ae35
Steven Flintham <sgf@lemma.co.uk>
2017-07-23 02:48:59 +0100

    Add TODOs

--- src/pc.py
+++ src/pc.py
@@ -5,6 +5,14 @@
 # intended to be assembled into a module has a '.a' extension. We therefore use
 # a '.sa' extension to distinguish (incompatible) standalone assembly output.
 
+# TODO: We could (eventually) "expect" a correlation between input filenames
+# and module names, and topologically sort the inputs to ensure "correct"
+# ordering - with an option to override this and use the order provided with no
+# assumptions about that correlation. As a halfway step, we could not check the
+# order but check (unless overridden) that every dependency has a corresponding
+# input file (i.e. check we have the expected set of files, but not check the
+# order).
+
 import argparse
 import os
 import re
@@ -66,6 +74,20 @@ if not init_list:
     # that - has no INIT, even if other modules do.
     die("No initialisation code to call!")
 
+# TODO: We should probably not actually be generating a single output file here
+# - I was thinking we should invoke 'acme' ourselves (I only hadn't got round
+# to that yet) and give it multiple files on the command line. We could do the
+# hackery on 32cmd.sa when we generate it as part of the "tool build". However,
+# would we have trouble generating the series of calls to _INIT_XXX? Well, not
+# trouble as such, but if we generated this as a "permanent" file, what name
+# would it have? Would we just shove it in a temporary file and discard it
+# afterwards? That's not ideal for debugging build problems where we'd like to
+# invoke acme manually. Maybe it would be best if we did the concatenation to a
+# single file, by default a temp file, but with a -S option to say "generate
+# the concatenated file and don't assemble it" for debugging - we could include
+# comments delimiting the various files which went into it. Let's not forget
+# that performance isn't really an issue - the extra time and disc space
+# required for the concatenated file creation is negligible.
 output_asm = 'foo' # SFTODO: need to decide what path/name to use for this
 with open(output_asm, 'w') as outfile:
     cat(outfile, 'vmsrc/plvmbb-pre.s')
