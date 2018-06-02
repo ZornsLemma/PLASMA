90df71026e418aad1b80ab35a9143ea18ab9b932
Steven Flintham <sgf@lemma.co.uk>
2018-06-02 02:26:13 +0100

    pt.py snapshot
    Not at the point of having assemble-able output yet, but this is looking
    fairly promising all the same. Have to go to bed unfortunately!

--- src/plasmatool/pt.py
+++ src/plasmatool/pt.py
@@ -22,27 +22,87 @@ def read_dci(f):
         s += chr(c)
     return s
 
+def dci_bytes(s):
+    result = ''
+    for c in s[0:-1]:
+        result += '$%02X,' % (ord(c) | 0x80)
+    result += '$%02X' % ord(s[-1])
+    return result
+
+class Label:
+    def __init__(self, name):
+        self.name = name
+
+    def acme_reference(self):
+        return "!WORD\t%s+0" % (self.name,)
+
+    def acme_rld(self, fixup_label, esd):
+        return ("\t!BYTE\t$81\t\t\t; INTERNAL FIXUP\n" +
+                "\t!WORD\t%s-_SEGBEGIN\n" +
+                "\t!BYTE\t$00") % (fixup_label.name,)
+
+class ExternalReference:
+    def __init__(self, external_name, offset):
+        self.external_name = external_name
+        self.offset = offset
+
+    def acme_reference(self):
+        return "!WORD\t%d\t\t\t; %s+%d" % (self.offset, self.external_name, self.offset)
+
+    def acme_rld(self, fixup_label, esd):
+        return ("\t!BYTE\t$91\t\t\t; EXTERNAL FIXUP\n" +
+                "\t!WORD\t%s-_SEGBEGIN\n" +
+                "\t!BYTE\t%d\t\t\t; ESD INDEX (%s)") % (fixup_label.name, esd.get_index(self.external_name), self.external_name)
+
+class ESD:
+    def __init__(self):
+        self.dict = {}
+
+    def get_index(self, external_name):
+        esd_entry = self.dict.get(external_name)
+        if esd_entry is None:
+            esd_entry = (0x10, len(self.dict))
+            self.dict[external_name] = esd_entry
+        return esd_entry[1]
+
+    def dump(self):
+        print(";\n; EXTERNAL/ENTRY SYMBOL DICTIONARY\n;")
+        # TODO: I think the current PLASMA VM will be fine, as it searches the whole ESD every
+        # time, but this should probably output the ESD entries in order of their index, not
+        # the arbitrary order they appear in the dictionary iteration.
+        for external_name, esd_entry in self.dict.items():
+            print("\t; DCI STRING: %s" % external_name)
+            print("\t!BYTE\t%s" % dci_bytes(external_name))
+            print("\t!BYTE\t$%02X\t\t\t; %s" % (esd_entry[0], "EXTERNAL SYMBOL FLAG" if esd_entry[0] == 0x10 else "ENTRY SYMBOL FLAG")) # TODO: Bit crap assuming if it's not 0x10 it's 0x08
+            print("\t!WORD\t%d\t\t\t; ESD INDEX" % (esd_entry[1]))
+        print("\t!BYTE\t$00\t\t\t; END OF ESD")
+
 class LabelledBlob:
     def __init__(self, blob):
         self.blob = blob
-        self.labels = [''] * len(self.blob)
-        self.label_references = [''] * len(self.blob)
+        self.labels = [[] for _ in range(len(self.blob))]
+        self.references = [None] * len(self.blob)
 
     def __getitem__(self, key):
         return self.blob[key]
 
-    def label(self, key, name):
-        assert not self.labels[key]
-        self.labels[key] = name
+    def label(self, key, lbl):
+        #print('QQQ %s' % (type(key)))
+        #print('YYY %d %s' % (key, lbl.name))
+        #print('AAA %r' % self.labels[key])
+        self.labels[key].append(lbl)
+        #print('BBB %r' % self.labels[key])
+        #print('ZZZ %d' % (len(self.labels[key])))
+        #print('QPP %r' % (self.labels))
 
     def label_or_get(self, key, name):
         if not self.labels[key]:
-            self.labels[key] = name
-        return self.labels[key]
+            self.labels[key].append(Label(name))
+        return self.labels[key][0]
 
-    def reference_label(self, key, name):
-        assert not self.label_references[key]
-        self.label_references[key] = name
+    def reference(self, key, reference):
+        assert not self.references[key]
+        self.references[key] = reference 
 
     def read_u16(self, key):
         # TODO: Best way to write this?!
@@ -54,15 +114,37 @@ class LabelledBlob:
     # module)
     def dump(self):
         i = 0
+        fixup_count = 0
+        fixups = []
         while i < len(self.blob):
-            if not self.label_references[i]:
-                print('%s\t!BYTE $%02X' % (self.labels[i], ord(self.blob[i])))
+            if not self.references[i]:
+                #print('SFTODO XXX %d %d %d' % (i, len(self.labels), len(self.labels[i])))
+                for label in self.labels[i]:
+                    print('%s' % label.name)
+                print('\t!BYTE\t$%02X' % (ord(self.blob[i]),))
             else:
-                print('%s\t!WORD %s $%02X, $%02X' % (self.labels[i], self.label_references[i], ord(self.blob[i]), ord(self.blob[i+1])))
+                reference = self.references[i]
+                assert not self.labels[i]
+                fixup_label = Label('_F%03d' % fixup_count)
+                fixup_count += 1
+                fixups.append((reference, fixup_label))
+                print('%s\t%s' % (fixup_label.name, reference.acme_reference()))
                 i += 1
                 assert not self.labels[i]
-                assert not self.label_references[i]
+                assert not self.references[i]
             i += 1
+
+        # TODO: Eventually we may want the blob to pass the RLD/ESD stuff it wants to emit to
+        # some other object; this might (though we'd need to be careful about INIT-combining)
+        # for example allow us to merge two modules into a single module.
+
+        print(";\n; RE-LOCATEABLE DICTIONARY\n;")
+        # TODO: Need to emit _C RLD entries
+        esd = ESD()
+        for reference, fixup_label in fixups:
+            print(reference.acme_rld(fixup_label, esd))
+        print("\t!BYTE\t$00\t\t\t; END OF RLD")
+        esd.dump()
 
 with open('../rel/PLASM#FE1000', 'rb') as f:
     seg_size = read_u16(f)
@@ -111,12 +193,15 @@ with open('../rel/PLASM#FE1000', 'rb') as f:
 org = 4094
 
 doing_code_table_fixups = True
+bytecode_function_labels = []
 for i, (rld_type, rld_word, rld_byte) in enumerate(rld):
     if rld_type == 0x02: # code table fixup
         assert doing_code_table_fixups
         assert rld_byte == 0
         blob_index = rld_word - org - blob_offset
-        blob.label(blob_index, '_C%03d' % i)
+        label = Label('_C%03d' % i)
+        bytecode_function_labels.append(label)
+        blob.label(blob_index, label)
         #print blob[blob_index]
     else:
         doing_code_table_fixups = False
@@ -126,23 +211,24 @@ for i, (rld_type, rld_word, rld_byte) in enumerate(rld):
         # for precise values for now.
         if rld_type == 0x91: # external fixup
             target_esd_index = rld_byte
-            label = None
+            reference = None
             for esd_name, esd_flag, esd_index in esd: # TODO: We could have a dictionary keyed on esd_index
                 if esd_index == target_esd_index:
                     # TODO: This is not really right; it will emit a !WORD _EFOO+4 thing in the
                     # dump, but these really are fundamentally different and need to emit just the '4'                     # but trigger emission an external fixup.
-                    label = '_E_%s+%d' % (esd_name, star_addr)
+
+                    reference = ExternalReference(esd_name, star_addr)
                     break
             assert label
-            blob.reference_label(addr, label)
+            blob.reference(addr, reference)
         elif rld_type == 0x81: # internal fixup
             assert rld_byte == 0
             blob_index = star_addr - org - blob_offset
             # TODO? label would be _C or _D in compiler output, we can't tell
             # and don't strictly care (I think). Perhaps use _I for internal?
-            # TODO: Perhaps have a separate count starting at 0 for these?
+            # TODO: Perhaps have a separate count starting at 0 for these? If so, label_or_get should probably be responsible for incrementing that count to avoid gaps.
             label = blob.label_or_get(blob_index, '_T%03d' % i)
-            blob.reference_label(addr, label)
+            blob.reference(addr, label)
         else:
             assert False
 
