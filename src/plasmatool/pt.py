f0b986da93acd6ea3b8545ac4ee45996017905d3
Steven Flintham <sgf@lemma.co.uk>
2019-04-20 03:25:18 +0100

    Add SFTODO

--- src/plasmatool/pt.py
+++ src/plasmatool/pt.py
@@ -600,6 +600,7 @@ class Instruction(ComparisonMixin):
         return (self.is_simple_load() and not self.has_side_effects()) or self.instruction_class == InstructionClass.CONSTANT
 
     def is_terminator(self):
+        # SFTODO: Is there a good reason this code is so different from is_store()/is_load()/etc?
         opdef = opdict.get(self.opcode, None)
         return opdef and opdef.get('terminator', False)
 
